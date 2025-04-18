ba8a4131776785073f3d53bae6c7edb03ba9e3c3
Abhay Singh <abhay97ps@gmail.com>
2025-04-18 06:10:49 -0400

    setup: aws x-ray logs

--- generic_chatbot/generic_chatbot/settings.py
+++ generic_chatbot/generic_chatbot/settings.py
@@ -74,6 +74,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "aws_xray_sdk.ext.django.middleware.XRayMiddleware",
 ]
 
 

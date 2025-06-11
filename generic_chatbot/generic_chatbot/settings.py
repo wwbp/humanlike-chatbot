86da3568dcd6bd8cce966d005ba42e92691d1750
Abhay Singh <abhay97ps@gmail.com>
2025-06-11 14:05:39 -0400

    Debug broken (#24)
    * test
    
    * test
    
    * test
    
    * locust load test file + dockerfile update use gunicorn workers
    
    * test
    
    * Voice A/B test V1 ready (#5)
    
    * Update Pipfile.lock to match Pipfile
    
    * added voice feature
    
    * working version of voice realtime interaction except data not being stored
    
    * still unsure how to save convo when done in realtime voice to voice mode
    
    * save data text version ready ish, need to check why utterance is not saved
    
    * fixed save to DB issue
    
    * voice-chat uploaded with FE cache issue
    
    * voice
    
    * voice bot feature dev upload
    
    * wildcard allowed hosts
    
    * fix: broken react app api url
    
    * redudant /api/ rm
    
    * hidden message display
    
    ---------
    
    Co-authored-by: Soojin Lee <71036622+Soojin-Lee0819@users.noreply.github.com>

--- generic_chatbot/generic_chatbot/settings.py
+++ generic_chatbot/generic_chatbot/settings.py
@@ -184,3 +184,18 @@ SESSION_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 CSRF_COOKIE_SECURE = True
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}

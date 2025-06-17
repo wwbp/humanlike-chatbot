7e88127a995877cc59283bde2f57daf4cde8dade
Abhay Singh <abhay97ps@gmail.com>
2025-06-17 14:29:48 -0400

    build: admin interface for bots

--- generic_chatbot/generic_chatbot/urls.py
+++ generic_chatbot/generic_chatbot/urls.py
@@ -15,16 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     path('', include('chatbot.urls')),  # Include chatbot app URLs directly
 ]
 
-from django.conf import settings
-from django.conf.urls.static import static
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)

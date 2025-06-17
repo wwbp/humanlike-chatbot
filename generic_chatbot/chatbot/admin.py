fdcf3e5f71987b5d1a8e1e8347871cf37d59745e
Abhay Singh <abhay97ps@gmail.com>
2025-06-17 14:41:24 -0400

    build: admin interface for bots (#30)

--- generic_chatbot/chatbot/admin.py
+++ generic_chatbot/chatbot/admin.py
@@ -1,2 +1,45 @@
 from django.contrib import admin
+from .models import Conversation, Utterance, Bot, Keystroke
 
+
+@admin.register(Conversation)
+class ConversationAdmin(admin.ModelAdmin):
+    list_display = (
+        "conversation_id", "bot_name", "participant_id",
+        "initial_utterance", "study_name", "user_group",
+        "survey_id", "started_time",
+    )
+    search_fields = (
+        "conversation_id", "bot_name", "participant_id",
+        "study_name", "user_group", "survey_id",
+    )
+    list_filter = ("bot_name", "user_group")
+    readonly_fields = ("started_time",)
+
+
+@admin.register(Utterance)
+class UtteranceAdmin(admin.ModelAdmin):
+    list_display = (
+        "conversation", "speaker_id", "bot_name",
+        "participant_id", "created_time", "is_voice",
+    )
+    search_fields = ("speaker_id", "bot_name", "participant_id", "text")
+    list_filter = ("is_voice", "speaker_id", "bot_name")
+    readonly_fields = ("created_time",)
+
+
+@admin.register(Bot)
+class BotAdmin(admin.ModelAdmin):
+    list_display = ("name", "model_type", "model_id")
+    search_fields = ("name", "model_type", "model_id")
+
+
+@admin.register(Keystroke)
+class KeystrokeAdmin(admin.ModelAdmin):
+    list_display = (
+        "conversation_id", "timestamp",
+        "keystroke_count", "total_time_on_page",
+        "total_time_away_from_page",
+    )
+    search_fields = ("conversation_id",)
+    list_filter = ("timestamp",)

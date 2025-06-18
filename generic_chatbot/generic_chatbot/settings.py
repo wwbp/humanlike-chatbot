694b5ef6f50f2c685f74f326052cbc3b2a89822c
Abhay Singh <abhay97ps@gmail.com>
2025-06-18 02:29:47 -0400

    Build admin interface (#35)
    * build: admin interface for bots
    
    * whitenoise static files for
    
    * fix: pipfile whitenoise
    
    * nit
    
    * /static whitenoise serve
    
    * allow csrf;
    
    * csrf setting

--- generic_chatbot/generic_chatbot/settings.py
+++ generic_chatbot/generic_chatbot/settings.py
@@ -82,10 +82,14 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = ["*"]
 
-CORS_ALLOWED_ORIGINS = ["*"]
-
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "https://dev.bot.wwbp.org",
+    "https://bot.wwbp.org",
+]
 
 ROOT_URLCONF = "generic_chatbot.urls"
 

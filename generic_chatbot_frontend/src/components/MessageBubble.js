177fca5882a0662f797082781abc9467bfa0f341
Abhay Singh <abhay97ps@gmail.com>
2025-06-30 19:04:15 -0400

    fix: do not display placeholder when avatar is none

--- generic_chatbot_frontend/src/components/MessageBubble.js
+++ generic_chatbot_frontend/src/components/MessageBubble.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const MessageBubble = ({ sender, content, avatar }) => (
   <div className={`message-row ${sender === "You" ? "sent" : "received"}`}>
-    {avatar.avatar_type!=="none" && sender !== "You" && (
+    {sender !== "You" && avatar?.image_base64 && (
       <img src={avatar.image_base64} alt="Avatar" className="message-avatar" />
     )}
     <div className={`message ${sender === "You" ? "sent" : "received"}`}>

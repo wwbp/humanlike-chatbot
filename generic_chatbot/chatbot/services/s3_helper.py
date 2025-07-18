e546bf1fc5ababf8c4407f984d0e33dd72d544d7
sourabh-swargam <55691793+sourabh-swargam@users.noreply.github.com>
2025-07-18 12:36:55 -0400

    updated s3 for local dev (#73)
    * added fallback to render conversation with avatar
    
    * updated avatar default param to none
    
    * updated image upload process - s3
    
    * removed unused env variables
    
    * Update bots.py
    
    * increased timeout for avatar generation delay
    
    * render image with url
    
    * added logging statements for debugging
    
    * corrected model migrations
    
    * added conditional avatar render
    
    * added chatbot condition query string param
    
    * fixed issue with dissimilar condition
    
    * dissimilar condition overwrite issue fix
    
    * avatar prompt update
    
    * updated s3 creds for local dev

--- generic_chatbot/chatbot/services/s3_helper.py
+++ generic_chatbot/chatbot/services/s3_helper.py
@@ -4,9 +4,17 @@ from PIL import Image
 import boto3
 import random
 
-s3 = boto3.client(
+if os.getenv("BACKEND_ENVIRONMENT") == "local":
+    s3 = boto3.client(
         's3',
-        region_name=os.getenv("AWS_REGION")
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        region_name=os.getenv('AWS_REGION'),
+    )
+else:
+    s3 = boto3.client(
+        's3',
+        region_name=os.getenv('AWS_REGION'),
     )
 
 def download(prefix, file_path):
